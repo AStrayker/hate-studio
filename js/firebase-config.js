f04e76441de0753de5b63e02f79eafa184047a18
AStrayker <astrajker@gmail.com>
2025-10-21 09:28:30 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,9 @@
-// Используем полные URL-адреса для импорта Firebase SDK
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// Ваша конфигурация Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyB5X84s9PgkmnKt9nsNlsl5-mRyI06hexM",
   authDomain: "hate-studio.firebaseapp.com",
@@ -19,5 +18,14 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+
+// Включение локального кэширования
+enableIndexedDbPersistence(db).catch((err) => {
+  if (err.code === 'failed-precondition') {
+    console.warn('Множественные вкладки открыты, кэширование отключено.');
+  } else if (err.code === 'unimplemented') {
+    console.warn('Кэширование не поддерживается в этом браузере.');
+  }
+});
 
 export { app, auth, db, storage };
