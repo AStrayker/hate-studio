9739dc61dc3340076a95832f5da9123a309f5b47
AStrayker <astrajker@gmail.com>
2025-10-21 09:08:39 +0300

    Update Firebase SDK imports to version 10.7.1

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,8 @@
 // Используем полные URL-адреса для импорта Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 // Ваша конфигурация Firebase
 const firebaseConfig = {
