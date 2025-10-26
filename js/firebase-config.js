48235baa566beece0eaff6029042e32528e33825
AStrayker <astrajker@gmail.com>
2025-10-26 22:48:42 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,7 +5,7 @@ import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.co
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3e9bkkUTazf8bKQOooXCMMCHkU5deKQE",
+  apiKey: "AIzaSyB5X84s9PgkmnKt9nsNlsl5-mRyI06hexM",
   authDomain: "hate-studio.firebaseapp.com",
   projectId: "hate-studio",
   storageBucket: "hate-studio.firebasestorage.app",
